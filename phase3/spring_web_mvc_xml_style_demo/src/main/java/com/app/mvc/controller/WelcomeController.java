1a7ff12380956aa8e6d545583ca01c794962471a
vinay.ingalahalli <vinay.ingalahalli@revature.com>
2020-08-14 13:44:24 +0530

    spring web mvc xml stye demo

---
+++ phase3/spring_web_mvc_xml_style_demo/src/main/java/com/app/mvc/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.app.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WelcomeController {
+
+	@RequestMapping("/")
+	public String sayHello() {
+		return "welcome";
+	}
+}
