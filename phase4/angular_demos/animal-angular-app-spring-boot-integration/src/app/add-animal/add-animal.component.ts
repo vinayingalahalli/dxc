efe6baa677d6b9b75e57dafcef23dcfd87ac1bec
vinay.ingalahalli <vinay.ingalahalli@revature.com>
2020-09-03 12:25:29 +0530

    angular animal spring boot app updated complete

--- phase4/angular_demos/animal-angular-app-spring-boot-integration/src/app/add-animal/add-animal.component.ts
+++ phase4/angular_demos/animal-angular-app-spring-boot-integration/src/app/add-animal/add-animal.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Animal } from '../animal';
+import { AnimalService } from '../animal.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-animal',
@@ -6,8 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-animal.component.css']
 })
 export class AddAnimalComponent implements OnInit {
+  private animal:Animal;
+  constructor(private animalService:AnimalService,private router:Router) {
+    this.animal=new Animal();
+   }
 
-  constructor() { }
+   public addAnimal():void{
+     this.animalService.addAnimal(this.animal).subscribe(res=>{
+       this.animal=new Animal();
+       this.router.navigate(['/animalsList']);
+     }
+      )
+   }
 
   ngOnInit() {
   }
